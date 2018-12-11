7fe73d614001fca3af04b927eefd608509ff01db
Stuart Whitehead <stuart.s.whitehead@gmail.com>
2018-12-11 16:52:09 -0500

    main work, fill out menu, lots of styling and content

--- js/main-dist.js
+++ js/main-dist.js
@@ -6,8 +6,9 @@ var pageNum = Math.floor(Math.random() * 14 + 1);
 // Save the API URL, get 15 beers on a page between 1-15
 var url = 'https://api.punkapi.com/v2/beers?page=' + pageNum + '&per_page=15';
 
-// Save reference to element we will append beer data to
-var ol = document.getElementById('beerList');
+// Save reference to elements we will append beer data to
+var menu = document.getElementById('beerList');
+var bottles = document.getElementById('beerBottles');
 
 // Send GET request
 fetch(url)
@@ -17,21 +18,27 @@ fetch(url)
 })
 // Do something with the data
 .then(function (data) {
-	console.log(data);
 	var beers = data;
+
+	// Map over the data, create an element for each beer and add it to the 'menu'
 	return beers.map(function (beer) {
-		console.log(beer.name);
-		var li = document.createElement('li'),
+		var menuLi = document.createElement('li'),
+		    bottleLi = document.createElement('li'),
 		    img = document.createElement('img'),
-		    title = document.createElement('span');
-		console.log('heyehe');
+		    title = document.createElement('span'),
+		    abv = document.createElement('span'),
+		    style = document.createElement('p');
 		img.src = beer.image_url;
 		title.innerHTML = beer.name;
+		abv.innerHTML = " : " + beer.abv + "%";
+		style.innerHTML = beer.tagline;
 
-		li.appendChild(img);
-		li.appendChild(title);
-		console.log(li);
-		ol.appendChild(li);
+		bottleLi.appendChild(img);
+		bottles.appendChild(bottleLi);
+		menuLi.appendChild(title);
+		menuLi.appendChild(abv);
+		menuLi.appendChild(style);
+		menu.appendChild(menuLi);
 	});
 })
 // In case of error in the API response
--- js/main.js
+++ js/main.js
@@ -4,8 +4,9 @@ const pageNum = Math.floor((Math.random() * 14) + 1);
 // Save the API URL, get 15 beers on a page between 1-15
 const url = 'https://api.punkapi.com/v2/beers?page=' + pageNum + '&per_page=15';
 
-// Save reference to element we will append beer data to
-const ol = document.getElementById('beerList');
+// Save reference to elements we will append beer data to
+const menu = document.getElementById('beerList');
+const bottles = document.getElementById('beerBottles');
 
 // Send GET request
 fetch(url)
@@ -13,21 +14,27 @@ fetch(url)
 .then((resp) => resp.json())
 // Do something with the data
 .then(function(data) {
-	console.log(data);
 	let beers = data;
+
+	// Map over the data, create an element for each beer and add it to the 'menu'
 	return beers.map(function(beer) {
-		console.log(beer.name);
-		let li = document.createElement('li'),
+		let menuLi = document.createElement('li'),
+			bottleLi = document.createElement('li'),
 			img = document.createElement('img'),
-			title = document.createElement('span');
-		console.log('heyehe');
+			title = document.createElement('span'),
+			abv = document.createElement('span'),
+			style = document.createElement('p');
 		img.src = beer.image_url;
 		title.innerHTML = beer.name;
+		abv.innerHTML = " : " + beer.abv + "%";
+		style.innerHTML = beer.tagline;
 
-		li.appendChild(img);
-		li.appendChild(title);
-		console.log(li);
-		ol.appendChild(li);
+		bottleLi.appendChild(img);
+		bottles.appendChild(bottleLi);
+		menuLi.appendChild(title);
+		menuLi.appendChild(abv);
+		menuLi.appendChild(style);
+		menu.appendChild(menuLi);
 	})
 })
 // In case of error in the API response
